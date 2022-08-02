7f2374a55d25cc881f09bbf96f388e0d23bf312e
Akihiro Orikasa <a.orikasa@pocosoft.jp>
2022-08-02 14:16:30 +0900

    Initial Commit

---
+++ FirebaseWithTCA/FirebaseWithTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseWithTCAApp.swift
+//  FirebaseWithTCA
+//
+//  Created by 折笠明弘 on 2022/08/02.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseWithTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FirebaseWithTCAUITests/FirebaseWithTCAUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FirebaseWithTCAUITestsLaunchTests.swift
+//  FirebaseWithTCAUITests
+//
+//  Created by 折笠明弘 on 2022/08/02.
+//
+
+import XCTest
+
+class FirebaseWithTCAUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
