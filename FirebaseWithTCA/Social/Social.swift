7f243b0f5b60be0158405591d1f11ee3f3049171
Akihiro Orikasa <a.orikasa@pocosoft.jp>
2022-08-15 18:13:03 +0900

    Firebase UI

--- FirebaseWithTCA/Social/Social.swift
+++ FirebaseWithTCA/Social/Social.swift
@@ -6,6 +6,7 @@
 //
 
 import ComposableArchitecture
+import FirebaseAuth
 import SwiftUI
 
 struct SocialState: Equatable {
---
+++ FirebaseWithTCA/Social/SocialClient.swift
@@ -0,0 +1,12 @@
+//
+//  SocialClient.swift
+//  FirebaseWithTCA
+//
+//  Created by Akihiro Orikasa on 2022/08/15.
+//
+
+import Foundation
+
+struct SocialClient {
+    
+}
